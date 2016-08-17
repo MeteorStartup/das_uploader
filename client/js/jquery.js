70d137c85dd0bdd60bf47dd3b66e96a6a22b81c3
june <juner830310@gmail.com>
2016-08-18 05:05:38 +0900

    highcharts

--- client/js/jquery.js
+++ client/js/jquery.js
@@ -11006,3 +11006,15 @@ if ( !noGlobal ) {
 
 return jQuery;
 }));
+
+// Borrowed from jQuery 1.8.3's source code
+jQuery.fn.extend({
+	live: function( types, data, fn ) {
+		if( window.console && console.warn ) {
+			//console.warn( "jQuery.live is deprecated. Use jQuery.on instead." );
+		}
+
+		jQuery( this.context ).on( types, this.selector, data, fn );
+		return this;
+	}
+});
